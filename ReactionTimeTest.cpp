0f9e44b615909e9eb778eacd06576b57619edef7
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-10-09 12:34:18 +0400

    Update ReactionTimeTest.cpp

--- ReactionTimeTest.cpp
+++ ReactionTimeTest.cpp
@@ -5,14 +5,19 @@ using namespace std;
 
 int main()
 {
+	Sleep(2000);
 	INPUT Input = { 0 };
 	Input.type = INPUT_MOUSE;
-
-	Input.mi.dx = -9500;
-	Input.mi.dy = 30000;
+	
+	POINT p;
+	GetCursorPos(&p);
+	cout << p.x <<"  " << p.y << endl;
+	Input.mi.dx = p.x;
+	Input.mi.dy = p.y;
 
 	// set move cursor directly
-	Input.mi.dwFlags = MOUSEEVENTF_MOVE | MOUSEEVENTF_ABSOLUTE;
+	//Input.mi.dwFlags = MOUSEEVENTF_MOVE;          /* map to entire virtual desktop */
+;
 
 	//mouse setup shit
 	INPUT in[4] = {};
@@ -22,9 +27,12 @@ int main()
 	in[1].type = 0;
 	in[1].mi.dwFlags = MOUSEEVENTF_LEFTUP;
 
-	
+
 	SendInput(1, &Input, sizeof(INPUT));
 
+	GetCursorPos(&p);
+	cout << p.x << "  " << p.y << endl;
+	
 	Sleep(100);
 
 	ofstream fout("RGB.txt", ofstream::app);
@@ -35,17 +43,16 @@ int main()
 	// The coordinates should be inside the max resolution of the device context 
 	// ex. screen coordinate X < 1919; in case of a full HD display 
 
-	POINT p;
-	GetCursorPos(&p);
 
-	while (1) { 
+	GetCursorPos(&p);
+	while (1) {
 		COLORREF c = GetPixel(dng, p.x, p.y);
-		if ((int)GetRValue(c) >= 65 && (int)GetRValue(c) <= 75) { //green or blue
+		if ((int)GetRValue(c) >= 65 && (int)GetRValue(c) <= 75) { //green
 			SendInput(2, in, sizeof(INPUT));        //press mouse
 			cout << "\n\n\n\n pressed\n\n\n\n";
 			Sleep(100);
 		}
-		
+
 		if (GetKeyState('Q') & 0x8000) { //q for quit
 			exit(0);
 		}
@@ -53,7 +60,7 @@ int main()
 		cout << (int)GetRValue(c) << ", ";
 		cout << (int)GetGValue(c) << ", ";
 		cout << (int)GetBValue(c) << ")" << endl;
-		
+
 	}
 	/*
 	fout << "RGB (";
@@ -67,7 +74,7 @@ int main()
 	cout << (int)GetGValue(c) << ", ";
 	cout << (int)GetBValue(c) << ")" << endl;
 	cout << "x: " << p.x << "     y:" << p.y << endl;
-	// Releasing the Handle 
+	// Releasing the Handle
 	*/
 	ReleaseDC(NULL, dng);
 }
