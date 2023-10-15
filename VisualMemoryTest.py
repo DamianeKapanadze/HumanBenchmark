46095cad3a31d077260fb975bbbef72ed5bd0494
DamianeKapanadze <75084003+DamianeKapanadze@users.noreply.github.com>
2023-10-15 23:52:59 +0400

    Update VisualMemoryTest.py

--- VisualMemoryTest.py
+++ VisualMemoryTest.py
@@ -1,24 +1,62 @@
-
+from multiprocessing import get_all_start_methods
 import pyautogui
 import time
 import cv2
 from pynput.keyboard import Key, Controller
 import numpy as np
+from PIL import Image
 
 
-
-def clickWhitePixels(s):
-    xlist = []
-    ylist = []
- 
-    for i in range(s.height-1):
-        for j in range(s.width-1):
-            if s.getpixel(i,j) == white and s.getpixel(i,j) != white:
-                xlist.append(i)
-                ylist.append(j)
+def click(s):
+    image = Image.open(r'C:\Users\dk4770\Desktop\screenshot1.png')
     
+    # Convert the image to RGB mode
+    image = image.convert('RGB')
+
+    # Get the width of the image
+    width, height = image.size
+
+    # Initialize a counter for color changes
+    size = 0
+
+    for j in range(width-1):
+        pixel_color = image.getpixel((j, 0))
+        #print(pixel_color)
+        if pixel_color != blue and image.getpixel((j+1, 0)) == blue:
+            size += 1
+
+    
+    
+    interval = min(width, height) // (size + 1)
+
+    # Initialize a list to store results
+    results = []
+
+    for i in range(1, size + 1):
+        for j in range(1, size + 1):
+            x = j * interval
+            y = i * interval
+
+            # Check if the pixel is white
+            pixel_color = image.getpixel((x, y))
+            results.append(is_white(pixel_color))
+            print(pixel_color)
+            if(is_white(pixel_color)): 
+                time.sleep(0.5)
+                pyautogui.click(763+x, 270+y)
+                print("click", x, " ", y)
+                time.sleep(0.5)
 
 
+    print(f"The intersections are white: {results}")
+    time.sleep(0.95)
+    gameMode()
+
+
+
+
+def is_white(pixel):
+    return pixel == (255, 255, 255)
 
 def mse(img1, img2):
     difference = cv2.subtract(img1, img2)
@@ -28,7 +66,23 @@ def mse(img1, img2):
         return True
     return False
 
+
+def gameMode():
+    a=1
+    while a==1:
+        a=2
+        s = pyautogui.screenshot(region=(763,270, 380, 380))
+        s = cv2.cvtColor(np.array(s), cv2.COLOR_RGB2BGR)
+        cv2.imwrite(r'C:\Users\dk4770\Desktop\screenshot1.png', s)
+        time.sleep(2)
+        click(s);
+        
+
+
+
+
 white = (255, 255, 255)
+blue = (45, 113, 190)
 pyautogui.click(593, 53)
 keyboard = Controller()
 keyboard.type("https://humanbenchmark.com/tests/memory")
@@ -45,16 +99,16 @@ while True:
     print(mse(menu, s))
     if(mse(menu, s)):
         pyautogui.click(950, 570)
-        time.sleep(0.4)
-        s = pyautogui.screenshot(region=(760,270, 382, 382))
+        time.sleep(0.6)
+        print("aa")
+        gameMode()
+        break
         
 
 
-        clickWhitePixels(s);
 
 
 cv2.destroyAllWindows()
-
 
 
 
